ede36e8bb1baa189dd0bb1bf75e9446d17676ecd
Jack Ojango <92569967+blvckpanda@users.noreply.github.com>
2022-12-01 18:34:53 +0300

    introduction to js

---
+++ JavaScript basics/variables, constants, comments.js
@@ -0,0 +1,15 @@
+//comments
+// single line comment
+/*
+double line coment */
+
+//variables
+
+var score = 75 //older way of assigning variables
+console.log(score)
+
+let age = 25 //es6 way of assigning variables
+console.log(age)
+
+const PI = 3.142 //constant variables
+console.log(PI)
